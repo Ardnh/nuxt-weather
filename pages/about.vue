1d224f2f722ebd51010b071040a373eff31da7e6
Ardan Hilal <ardanhilal@pop-os.localdomain>
2023-06-01 11:15:38 +0700

    update about page

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-wrap justify-content-center md:align-items-center md:mt-0 mt-8 align-items-start w-full h-screen">
         <div class="flex flex-wrap justify-content-center align-items-end">
             <img src="/nuxt.svg" width="100">
-            <div class="text-7xl font-bold">Nuxt</div>
+            <div class="text-7xl font-bold ml-2">Nuxt</div>
         </div>
     </div>
 </template>
